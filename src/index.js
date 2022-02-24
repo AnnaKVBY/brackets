a86106fc18d2ee853774ac8ac9e47d416676085e
Anastasiya <annabycreate@gmail.com>
2022-02-24 23:22:32 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let changed = bracketsConfig.join('').replace(/,/g,'');
+  for (let s of str) {
+    let symbInd = changed.indexOf(s);
+    if (symbInd % 2 === 0) {
+      if (s === changed[symbInd+1] && stack[stack.length-1] === symbInd){
+        stack.pop();
+      } else if (s === changed[symbInd+1] && stack[stack.length-1] !== symbInd){
+        stack.push(symbInd)
+      } else {
+        stack.push(symbInd)
+      }
+     
+    } else if (stack.pop() != symbInd-1) {
+      return false;
+    }
+
+    }
+
+    return stack.length === 0;
+
 }
